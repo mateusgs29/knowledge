31febf4d3f0a56b906d39a45b6c93f5074c50e85
Mateus Gomes <mateus.gs1999@gmail.com>
2021-08-26 21:48:34 -0300

    adicionado ao consign rotas, validacoes e api

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,14 @@
 const app = require('express')()
+const consign = require('consign')
+const db = require('./config/db')
 
-const consign =  require('consign')
- 
+app.db = db
+
 consign()
   .then('./config/middlewares.js')
+  .then('./api/validation.js')
+  .then('./api')
+  .then('./config/routes.js')
   .into(app)
 
 app.listen(3000, () => {
