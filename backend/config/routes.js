8f8b4177888e2c466cd7bf2128037c8b8e7abcb8
Mateus Gomes <mateus.gs1999@gmail.com>
2021-08-26 21:49:02 -0300

    rotas do usuario

---
+++ backend/config/routes.js
@@ -0,0 +1,9 @@
+module.exports = app => {
+  app.route('/users')
+    .post(app.api.user.save)
+    .get(app.api.user.get)
+
+  app.route('/users/:id')
+    .put(app.api.user.save)
+    .get(app.api.user.getById)
+}
