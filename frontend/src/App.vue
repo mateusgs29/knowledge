31686ae25fbe1f106416ccdca8d50c36400a76ae
Mateus Gomes <mateus.gs1999@gmail.com>
2021-08-28 16:01:25 -0300

    grid e componentes de template

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,15 +1,52 @@
 <template>
-	<div id="app">
-		<h1>Versão Inicial</h1>
+	<div id="app" :class="{'hide-menu': !isMenuVisible}">
+		<Header title="Base de Conhecimento" :hideToggle="false" />
+		<Menu />
+		<Content />
+		<Footer />
 	</div>
 </template>
 
 <script>
+import { mapState } from 'vuex'
+import Header from '@/components/template/Header.vue'
+import Menu from '@/components/template/Menu.vue'
+import Content from '@/components/template/Content.vue'
+import Footer from '@/components/template/Footer.vue'
+
 export default {
 	name: "App",
+	components: { Header, Menu, Content, Footer },
+	computed: mapState(['isMenuVisible'])
 }
 </script>
 
 <style>
+	* {
+		font-family: "Lato", sans-serif;
+	}
 
+	body {
+		margin: 0;
+	}
+
+	#app {
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+
+		height: 100vh;
+		display: grid;
+		grid-template-rows: 60px 1fr 40px;
+		grid-template-columns: 300px 1fr;
+		grid-template-areas: 
+			"header header"
+			"menu content"
+			"menu footer";
+	}
+	#app.hide-menu {
+		grid-template-areas: 
+			"header header"
+			"content content"
+			"footer footer";
+	}
 </style>
