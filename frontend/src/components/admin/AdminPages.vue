9efad42d5d18661e73d58f8f6c28ab33f5c18124
Mateus Gomes <mateus.gs1999@gmail.com>
2021-09-01 09:47:49 -0300

    Pagina de administracao

---
+++ frontend/src/components/admin/AdminPages.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="admin-pages">
+    <PageTitle icon="fa fa-cogs" main="Administração do Sistema"
+      sub="Cadastro & Cia" />
+    <div class="admin-pages tabs">
+      <b-card no-body>
+        <b-tabs card>
+          <b-tab title="Artigos" active>
+            <ArticleAdmin />
+          </b-tab>
+          <b-tab title="Categorias">
+            <CategoryAdmin />
+          </b-tab>
+          <b-tab title="Usuários">
+            <UserAdmin />
+          </b-tab>
+        </b-tabs>
+      </b-card>
+    </div>
+  </div>
+</template>
+
+<script>
+import PageTitle from '../template/PageTitle.vue'
+import ArticleAdmin from './ArticleAdmin.vue'
+import CategoryAdmin from './CategoryAdmin.vue'
+import UserAdmin from './UserAdmin.vue'
+
+export default {
+  name: 'AdminPages',
+  components: { PageTitle, ArticleAdmin, CategoryAdmin, UserAdmin }
+}
+</script>
+
+<style>
+
+</style>
