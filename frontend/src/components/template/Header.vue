4d03736c0131b937f21fbb4b47779b3b76281791
Mateus Gomes <mateus.gs1999@gmail.com>
2021-09-01 09:49:08 -0300

    dropdown de usuario adicionado

--- frontend/src/components/template/Header.vue
+++ frontend/src/components/template/Header.vue
@@ -4,17 +4,22 @@
       <i class="fa fa-lg" :class="icon"></i>
     </a>
     <h1 class="title">
-      {{ title }}
+      <router-link to="/">{{ title }}</router-link>
     </h1>
+    <UserDropdown v-if="!hideUserDropdown" />
   </header>
 </template>
 
 <script>
+import UserDropdown from './UserDropdown.vue'
+
 export default {
   name: 'Header',
+  components: { UserDropdown },
   props: {
     title: String,
-    hideToggle: Boolean
+    hideToggle: Boolean,
+    hideUserDropdown: Boolean
   },
   computed: {
     icon() {
@@ -38,6 +43,7 @@ export default {
     justify-content: center;
     align-items: center;
   }
+
   .title {
     font-size: 1.2rem;
     color: #fff;
@@ -45,10 +51,16 @@ export default {
     flex-grow: 1;
     text-align: center;
   }
+
   .title a {
     color: #fff;
     text-decoration: none;
   }
+
+  .title a:hover {
+    color: #fff;
+    text-decoration: none;
+  }  
 
   header.header > a.toggle {
     width: 60px;
@@ -61,7 +73,9 @@ export default {
     align-items: center;
     cursor: pointer;
   }
+
   header.header > a.toggle:hover {
     background-color: rgba(0, 0, 0, 0.2);
+    color: #fff;
   }
 </style>
