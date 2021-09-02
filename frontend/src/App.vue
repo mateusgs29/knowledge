30fbb27cf8147d5d49b7be6eb379f5d69fbd3ba0
Mateus Gomes <mateus.gs1999@gmail.com>
2021-09-01 22:54:12 -0300

    metodos de validacao de token

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,25 +1,66 @@
 <template>
-	<div id="app" :class="{'hide-menu': !isMenuVisible}">
+	<div id="app" :class="{'hide-menu': !isMenuVisible || !user}">
 		<Header title="Base de Conhecimento" 
-			:hideToggle="false"
-			:hideUserDropdown="false" />
-		<Menu />
-		<Content />
+			:hideToggle="!user"
+			:hideUserDropdown="!user" />
+		<Menu v-if="user" />
+		<Loading v-if="validatingToken" />
+		<Content v-else />
 		<Footer />
 	</div>
 </template>
 
 <script>
+import axios from 'axios'
+import { baseApiUrl, userKey } from '@/global'
 import { mapState } from 'vuex'
 import Header from '@/components/template/Header.vue'
 import Menu from '@/components/template/Menu.vue'
 import Content from '@/components/template/Content.vue'
 import Footer from '@/components/template/Footer.vue'
+import Loading from '@/components/template/Loading.vue'
 
 export default {
 	name: "App",
-	components: { Header, Menu, Content, Footer },
-	computed: mapState(['isMenuVisible'])
+	components: { Header, Menu, Content, Footer, Loading },
+	computed: mapState(['isMenuVisible', 'user']),
+	data: function() {
+		return {
+			validatingToken: true
+		}
+	},
+	methods: {
+		async validateToken() {
+			this.validatingToken = true
+
+			const json = localStorage.getItem(userKey)
+			const userData = JSON.parse(json)
+			this.$store.commit('setUser', null)
+
+			if(!userData) {
+				this.validatingToken = false
+				this.$router.push({ name: 'auth' })
+				return 
+			}
+
+			const res = await axios.post(`${baseApiUrl}/validateToken`, userData)
+
+			if (res.data) {
+				this.$store.commit('setUser', userData)
+				if(this.$mq === 'xs' || this.$mq === 'sm') {
+					this.$store.commit('toggleMenu', false)
+				}
+			} else {
+				localStorage.removeItem(userKey)
+				this.$router.push({ name: 'auth' })
+			}
+
+			this.validatingToken = false
+		}
+	},
+	created() {
+		this.validateToken()
+	}
 }
 </script>
 
