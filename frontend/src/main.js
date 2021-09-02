3abd5426f75ec7e91f742b181057a4bf349926df
Mateus Gomes <mateus.gs1999@gmail.com>
2021-09-01 22:55:14 -0300

    adicionado importacoes de configuracao

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,13 +4,14 @@ import Vue from 'vue'
 import App from './App'
 
 import './config/bootstrap'
+import './config/msgs'
+import './config/axios'
+import './config/mq'
+
 import store from './config/store'
 import router from './config/router'
 
 Vue.config.productionTip = false
-
-// TEMPORARIO
-require('axios').defaults.headers.common['Authorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6Ik1hdGV1cyBHb21lcyIsImVtYWlsIjoibWF0QHRlc3RlLmNvbSIsImFkbWluIjp0cnVlLCJpYXQiOjE2MzA0NTMzMDQsImV4cCI6MTYzMDcxMjUwNH0.HivKzGPsPLe9NGTdDUUZFzlMoWRxAtaIjeWHL-nbrbU'
 
 new Vue({
   store,
